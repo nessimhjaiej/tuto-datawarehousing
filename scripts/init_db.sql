daeb6c0c283ca3c7be0b8a3faf0f716e798f1262
nessimhjaiej <157981368+nessimhjaiej@users.noreply.github.com>
2025-09-22 17:23:36 +0100

    Create init_db.sql

---
+++ scripts/init_db.sql
@@ -0,0 +1,9 @@
+/*this script is for creating the database 
+and the schemas (bronze , silver , gold ) 
+warning : only run this if you don't have the 'datawarehouse' database 
+*/
+
+create database datawarehouse ; 
+create schema bronze; 
+create schema silver;
+create schema gold ; 
