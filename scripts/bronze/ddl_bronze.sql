3256a29aba97a7c1ed7510f7442d4977df05aae9
nessimhjaiej <157981368+nessimhjaiej@users.noreply.github.com>
2025-09-24 12:41:32 +0100

    Add files via upload

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,55 +1,90 @@
-use bronze ; 
--- Create the table
-drop table if exists crm_cust_info ; 
-CREATE TABLE crm_cust_info (
-    cst_id INT PRIMARY KEY,
-    cst_key VARCHAR(20),
-    cst_firstname VARCHAR(50),
-    cst_lastname VARCHAR(50),
-    cst_marital_status CHAR(1), 
-    cst_gndr CHAR(1),            
-    cst_create_date DATE
-);	
-drop table if exists crm_prd_info ; 
-CREATE TABLE crm_prd_info (
-    prd_id INT PRIMARY KEY,
-    prd_key VARCHAR(50),
-    prd_nm VARCHAR(255),
-    prd_cost DECIMAL(10,2),      -- allows numbers with 2 decimals (cost can be NULL)
-    prd_line CHAR(1),            -- short code like 'R'
-    prd_start_dt DATE,
-    prd_end_dt DATE
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
 );
-drop table if exists crm_sales_details ; 
-CREATE TABLE sales_details (
-    sls_ord_num   VARCHAR(20) PRIMARY KEY,   -- order number like SO43697
-    sls_prd_key   VARCHAR(50),               -- links to product key
-    sls_cust_id   INT,                       -- links to customer id
-    sls_order_dt  DATE,
-    sls_ship_dt   DATE,
-    sls_due_dt    DATE,
-    sls_sales     DECIMAL(10,2),
-    sls_quantity  INT,
-    sls_price     DECIMAL(10,2)
+GO
+
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
+
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
 );
-drop table if exists erp_cust_az12  ; 
-CREATE TABLE erp_cust_az12 (
-    cid    VARCHAR(20) PRIMARY KEY,  -- customer code/key
-    bdate  DATE,                     -- birthdate
-    gen    VARCHAR(10)               -- gender (e.g. 'Male', 'Female')
+GO
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
+
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
 );
-drop table if exists erp_loc_a101 ; 
-CREATE TABLE erp_loc_a101 (
-    cid    VARCHAR(20) PRIMARY KEY,
-    cntry  VARCHAR(100)   -- country name
+GO
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
 );
-drop table if exists erp_px_cat_g1v2 ; 
-CREATE TABLE erp_px_cat_g1v2 (
-    id          VARCHAR(20) PRIMARY KEY,
-    cat         VARCHAR(100),   -- category
-    subcat      VARCHAR(100),   -- subcategory
-    maintenance VARCHAR(10)     -- Yes / No
+GO
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
 );
--- i used this because i made mistakes naming the tables when creating them 
--- alter table loc_a101 rename to erp_loc_a101; 
--- alter table px_cat_g1v2 rename to erp_px_cat_g1v2 ; 
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
