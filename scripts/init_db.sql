49a06bf5a96080f30e5adb3a13fb61ebb6fbcef8
nessimhjaiej <157981368+nessimhjaiej@users.noreply.github.com>
2025-09-24 12:40:38 +0100

    Update init_db.sql

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -3,7 +3,30 @@ and the schemas (bronze , silver , gold )
 warning : only run this if you don't have the 'datawarehouse' database 
 */
 
-create database datawarehouse ; 
-create schema bronze; 
-create schema silver;
-create schema gold ; 
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
